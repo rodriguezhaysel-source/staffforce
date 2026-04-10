668dd4aa0fb98102b3898c589f77165dac478a3c
rodriguezhaysel-source <rodriguezhaysel@gmail.com>
2026-04-10 06:42:03 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  ...(process.env.CAPACITOR_BUILD === 'true' && {
-    output: 'export',
-    trailingSlash: true,
-    images: { unoptimized: true },
-  })
-}
-module.exports = nextConfig
