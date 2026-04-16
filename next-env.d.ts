4466e8ac8d668cfa556168af4a46357309adc965
rodriguezhaysel-source <rodriguezhaysel@gmail.com>
2026-04-15 20:18:42 -0400

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/pages/api-reference/typescript for more information.
